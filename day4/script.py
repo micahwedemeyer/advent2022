b0870f4534d9ccb9c1961287ba5283470a89e779
Micah Wedemeyer <me@micahwedemeyer.com>
2022-12-31 11:23:03 -0500

    Day 4 part 2

--- day4/script.py
+++ day4/script.py
@@ -3,5 +3,5 @@ from day4.elf_pair import ElfPair
 input = open("input.txt").read().splitlines()
 pairs = [ElfPair(line) for line in input]
 
-overlap = len([p for p in pairs if p.has_full_overlap()])
+overlap = len([p for p in pairs if p.has_overlap()])
 print(overlap)
